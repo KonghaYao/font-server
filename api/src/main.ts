fc438ccb16f6c636ee5ba48dc35b96a905a5e61f
江夏尧 <3446798488@qq.com>
2023-05-20 22:58:33 +0000

    feat(api): 添加根路径访问

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,6 +12,9 @@ const app = new Koa();
 const router = new Router();
 
 router
+    .get("/", (ctx) => {
+        ctx.body = "欢迎使用 Font Server 提供的服务";
+    })
     .use(FontsRouter.routes())
     .use(SplitRouter.routes())
     .use(WebHookRouter.routes());
